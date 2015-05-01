4c8a204d071d02e7f0cfd91d39cb6060aa5f2f4f
nipoto <nipoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-01 05:41:55 +0000

    readme file edited ...
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-count-orders-in-adminbar/trunk@1150782 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		Plugin Name: WC Pending Orders Count in AdminBar
 		Plugin URI: http://ideyeno.ir
 		Description: نمایش تعداد سفارشات در انتظار تحویل ووکامرس در منوی ادمین وردپرس ...
-		Version: 1.0.2
+		Version: 1.0.3
 		Author: Nima Saberi
 		Author URI: http://ideyeno.ir
 	*/
